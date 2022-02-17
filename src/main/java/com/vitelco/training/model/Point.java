5c21a90cca4d50d1a41b7a33ba38a2e1d67011ae
ob0420 <ogulgerek.babakulyyeva@bil.omu.edu.tr>
2022-02-17 21:02:59 +0300

    Initial Commit

---
+++ src/main/java/com/vitelco/training/model/Point.java
@@ -0,0 +1,14 @@
+package com.vitelco.training.model;
+
+public class Point {
+    public int x = 0;
+    public int y = 0;
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
---
+++ src/main/java/com/vitelco/training/model/Ractangle.java
@@ -0,0 +1,19 @@
+package com.vitelco.training.model;
+
+public class Ractangle {
+    //pointA pointB pointC pointD
+    public Point pointA;
+    public Point pointB;
+    public Point pointC;
+    public Point pointD;
+
+    @Override
+    public String toString() {
+        return "Ractangle{" +
+                "pointA=" + pointA +
+                ", pointB=" + pointB +
+                ", pointC=" + pointC +
+                ", pointD=" + pointD +
+                '}';
+    }
+}
